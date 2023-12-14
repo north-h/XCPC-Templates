fbc23ae135c5f6dccd486878079c0b741715a157
north_h <1635273401@qq.com>
2023-12-14 22:39:16 +0800

    修改简单线段树的模板

--- 数据结构/线段树（朴素）.cpp
+++ 数据结构/线段树（朴素）.cpp
@@ -1,39 +1,73 @@
-const int N = 100010；
+template<class T>
+struct SegmentTree {
+    struct Node {
+        int l, r;
+        T sum, lazy;
+    };
 
-struct Node {
-    int l, r, sum;
-} tr[N * 4];
+    vector<Node> tr;
+    vector<T> a;
 
-int a[N];
-
-void pushup(int u) {
-    tr[u].sum = tr[u << 1].sum + tr[u << 1 | 1].sum;
-}
-void build(int u, int l, int r) {
-    tr[u] = {l, r, a[l], 0};
-    if (l == r)return;
-    int mid = l + r >> 1;
-    pushdown(u);
-    build(u << 1, l, mid);
-    build(u << 1 | 1, mid + 1, r);
-    pushup(u);
-}
-//单点修改
-void modify(int u, int x, int k) {
-    if (tr[u].l == tr[u].r) {
-        tr[u].sum += k;
-        return;
+    SegmentTree(const vector<T> &arr, const int n) {
+        tr.resize(n * 4);
+        a = arr;
+        build(1, 1, n);
     }
-    int mid = tr[u].l + tr[u].r >> 1;
-    if (x <= mid)modify(u << 1, x, k);
-    else modify(u << 1 | 1, x, k);
-}
-//区间查询
-int query(int u, int l, int r) {
-    if (tr[u].l >= l && tr[u].r <= r) return tr[u].sum;
-    int sum = 0;
-    int mid = tr[u].l + tr[u].r >> 1;
-    if (l <= mid)sum += query(u << 1, l, r);
-    if (r > mid) sum += query(u << 1 | 1, l, r);
-    return sum;
-}
+
+    void pushup(int u) {
+        tr[u].sum = tr[u << 1].sum + tr[u << 1 | 1].sum;
+    }
+
+    void pushdown(int u) {
+        if (tr[u].lazy) {
+            tr[u << 1].sum += tr[u].lazy * (tr[u << 1].r - tr[u << 1].l + 1);
+            tr[u << 1 | 1].sum += tr[u].lazy * (tr[u << 1 | 1].r - tr[u << 1 | 1].l + 1);
+            tr[u << 1].lazy += tr[u].lazy;
+            tr[u << 1 | 1].lazy += tr[u].lazy;
+            tr[u].lazy = 0;
+        }
+    }
+
+    void build(int u, int l, int r) {
+        tr[u] = {l, r, a[l], 0};
+        if (l == r)return;
+        int mid = l + r >> 1;
+        pushdown(u);
+        build(u << 1, l, mid);
+        build(u << 1 | 1, mid + 1, r);
+        pushup(u);
+    }
+    //区间修改
+    void modify(int u, int l, int r, int k) {
+        if (tr[u].l >= l && tr[u].r <= r) {
+            tr[u].sum += (tr[u].r - tr[u].l + 1) * k;
+            tr[u].lazy += k;
+            return;
+        }
+        pushdown(u);
+        int mid = tr[u].l + tr[u].r >> 1;
+        if (l <= mid)modify(u << 1, l, r, k);
+        if (r > mid) modify(u << 1 | 1, l, r, k);
+        pushup(u);
+    }
+    //单点修改
+    void modify(int u, int x, int k) {
+        if (tr[u].l == tr[u].r) {
+            tr[u].sum += k;
+            return;
+        }
+        int mid = tr[u].l + tr[u].r >> 1;
+        if (x <= mid)modify(u << 1, x, k);
+        else modify(u << 1 | 1, x, k);
+    }
+    //区间查询
+    T query(int u, int l, int r) {
+        if (tr[u].l >= l && tr[u].r <= r) return tr[u].sum;
+        pushdown(u);
+        int sum = 0;
+        int mid = tr[u].l + tr[u].r >> 1;
+        if (l <= mid)sum += query(u << 1, l, r);
+        if (r > mid) sum += query(u << 1 | 1, l, r);
+        return sum;
+    }
+};
