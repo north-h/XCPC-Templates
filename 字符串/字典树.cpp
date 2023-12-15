6c243b99f3cdc29db3ce36c275e576c076ff4d32
north_h <1635273401@qq.com>
2023-12-15 16:40:32 +0800

    north_h

--- 字符串/字典树.cpp
+++ 字符串/字典树.cpp
@@ -1,26 +1,24 @@
-template<class T>
 struct Trie {
-    T idx = 0;
-    vector<vector<T>> s;
-    vector<T>cnt;
-    Trie(T n): cnt(n, 0), s(n, vector<T>(26, 0)) {}
+    int idx = 0;
+    int s[100010][26], cnt[100010];
+    bool exit[100010];
     void insert(string str) {
-        T p = 0;
+        int p = 0;
         for (int i = 0; i < str.size(); i++) {
-            T u = str[i] - 'a';
+            int u = str[i] - 'a';
             if (!s[p][u]) s[p][u] = ++idx;
             p = s[p][u];
+            cnt[p]++;
         }
-        cnt[p]++;
+        exit[p] = 1;
     }
 
-    T query(string str) {
-        T p = 0;
+    int query(string str) {
+        int p = 0;
         for (int i = 0; i < str.size(); i++) {
-            T u = str[i] - 'a';
+            int u = str[i] - 'a';
             if (!s[p][u]) return 0;
             p = s[p][u];
         }
         return cnt[p];
     }
-};
